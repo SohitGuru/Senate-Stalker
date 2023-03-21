d6399ad8fd144b3b9965add695608f3915592818
Arjun Khanna <ak2243@cornell.edu>
2023-03-21 17:33:01 -0400

    Reorganized scraper.ml and scraper.mli
    - Created a Parser module type
    - Made a module of type Parser for committee assignment parsing
    - General organization and cleanup

--- src/scraper.mli
+++ src/scraper.mli
@@ -7,3 +7,14 @@ val content_to_string : string -> string
 val content_to_file : string -> string -> unit
 (** [content_to_file url file] saves the content of the webpage at [url] and
     saves it to [file] *)
+
+module type Parser = sig
+  val url : string
+
+  type return
+  type input
+
+  val exec : input -> return
+end
+
+module Committees : Parser
