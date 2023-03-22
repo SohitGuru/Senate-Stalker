9ab27a0c8050f4916e55470a51c6d366f4466ac5
Sohit Gurung <sg857@cornell.edu>
2023-03-22 19:04:33 -0400

    Started development of execute function; will need fixing later

--- src/executor.ml
+++ src/executor.ml
@@ -2,5 +2,21 @@ exception BadArgument
 exception UnexpectedError
 
 open Command
+open Scraper
 
-let execute (cmd : command) = failwith "Unimplemented" (* TODO *)
+let execute (cmd : command) =
+  match cmd with
+  | Quit -> exit 0
+  | List -> Scraper.Members.exec () |> List.map
+  | Fetch (x, y) -> (
+      match x with
+      | Name -> [ "" ]
+      | Party -> [ "" ]
+      | State -> [ "" ]
+      | Address -> [ "" ]
+      | Phone -> [ "" ]
+      | Email -> [ "" ]
+      | Website -> [ "" ]
+      | Class -> [ "" ]
+      | Committees -> Scraper.Committees.exec (String.concat "" y)
+      | _ -> raise BadArgument)
