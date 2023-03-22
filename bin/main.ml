42fd4f883c2101f449ad0ffd6e5b820c95c347b2
Arjun Khanna <ak2243@cornell.edu>
2023-03-22 11:49:33 -0400

    Prompt Interface work
    - Created the basis of the prompt interface
    - Future work is dependent on how we want to structure the handling of commands

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,37 @@
 (* [run_stalking s] starts the web scraping of Senator*)
 let run_stalking s = raise (Failure "Error!")
 
+let help_message =
+  "To get a list of senators, please enter the command \'list\'\n\
+   To get information on a specific senator, please enter the field you want, \
+   followed by the senator's full name.\n\
+   Supported information fields are: \n\
+  \    state\n\
+  \    party\n\
+  \    committees\n"
+
+let prompt = "> "
+let handle cmd = ""
+(* TODO: the above needs to be implemented *)
+
+let rec prompter () =
+  print_string prompt;
+  match read_line () with
+  | exception End_of_file -> ()
+  | "exit" -> ()
+  | st ->
+      print_string (handle st);
+      prompter ()
+
 (** [main ()] prompts for the Senator name, then starts finding information
     about said Senator. *)
 let main () =
   ANSITerminal.print_string [ ANSITerminal.blue ]
     "\n\nSenate Stalker: Learn more about our Senators!\n";
-  print_endline
-    "Please enter the name of the Senator you wish to learn more about.\n";
-  print_string "> ";
-  match read_line () with
-  | exception End_of_file -> ()
-  | file_name -> run_stalking "Senator"
+  ANSITerminal.print_string [ ANSITerminal.yellow ] help_message;
+  prompter ();
+  ANSITerminal.print_string [ ANSITerminal.red ]
+    "\nThank you for using Senate Stalker\n"
 
 (* Execute finding Senator following input. *)
 let () = main ()
