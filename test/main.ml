cef661fa934f9a78323c82d724d8e128efab360d
Arjun Khanna <ak2243@cornell.edu>
2023-05-09 23:08:13 -0400

    Added executor tests in OUnit

--- test/main.ml
+++ test/main.ml
@@ -1,6 +1,8 @@
 open OUnit2
 open Senate
 open Scraper
+open Command
+open Executor
 
 let senate_members_url =
   "https://www.senate.gov/general/contact_information/senators_cfm.xml"
@@ -33,8 +35,45 @@ let markdown_tests =
         "snip snip" );
   ]
 
+let make_fetch_test (name : string) (command : command) (result : string list) =
+  name >:: fun _ -> assert_equal (execute command) result
+
+let fetch_tests =
+  [
+    make_fetch_test "fetch name test"
+      (Fetch (Name, [ "Sanders"; "Bernard" ]))
+      [ "Bernard Sanders" ];
+    make_fetch_test "fetch party test"
+      (Fetch (Party, [ "Booker"; "Cory A." ]))
+      [ "D" ];
+    make_fetch_test "fetch state test"
+      (Fetch (State, [ "Sanders"; "Bernard" ]))
+      [ "VT" ];
+    make_fetch_test "fetch address test"
+      (Fetch (Address, [ "Schumer"; "Charles E." ]))
+      [ "322 Hart Senate Office Building Washington DC 20510" ];
+    make_fetch_test "fetch phone test"
+      (Fetch (Phone, [ "Baldwin"; "Tammy" ]))
+      [ "(202) 224-5653" ];
+    make_fetch_test "fetch address test"
+      (Fetch (Email, [ "Padilla"; "Alex" ]))
+      [ "https://www.padilla.senate.gov/contact/" ];
+    make_fetch_test "fetch class test"
+      (Fetch (Class, [ "Warren"; "Elizabeth" ]))
+      [ "Class I" ];
+    make_fetch_test "fetch committees test"
+      (Fetch (Committees, [ "Sanders"; "Bernard" ]))
+      [
+        "Committee on Energy and Natural Resources";
+        "Committee on Environment and Public Works";
+        "Committee on Health, Education, Labor, and Pensions";
+        "Committee on the Budget";
+        "Committee on Veterans' Affairs";
+      ];
+  ]
+
 let tests =
   "test suite for project"
-  >::: List.flatten [ scraper_tests; parser_tests; markdown_tests ]
+  >::: List.flatten [ scraper_tests; parser_tests; markdown_tests; fetch_tests ]
 
 let _ = run_test_tt_main tests
