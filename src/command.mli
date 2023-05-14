3780af9ef4595b4e9106309810da781983e9af09
Arjun Khanna <ak2243@cornell.edu>
2023-05-14 04:09:55 -0400

    Cleaned up command.mli documentation

--- src/command.mli
+++ src/command.mli
@@ -1,8 +1,10 @@
-(** Parsing of user commands *)
+(** The [Command] module handles parsing of user commands into a variant type *)
 
 type arg_phrase = string list
 (** the type [cmd] represents the user's command and the arguments they provided *)
 
+(** the type [field] represents all the information fields a user might request
+    of a specific senator *)
 type field =
   | Name
   | Party
@@ -13,9 +15,8 @@ type field =
   | Website
   | Class
   | Committees
-      (** the type [field] represents all the information fields a user might
-          request of a specific senator *)
 
+(** [command] represents the command once parsed *)
 type command =
   | Fetch of field * arg_phrase
   | List
@@ -23,7 +24,10 @@ type command =
   | Export of string * arg_phrase
 
 exception Empty
+(** [Empty] is raised for empty commands *)
+
 exception Invalid
+(** [Invalid] is raised for badly formatted commands *)
 
 val parse : string -> command
 (** Parses the command. A command should either be quit or list with no
