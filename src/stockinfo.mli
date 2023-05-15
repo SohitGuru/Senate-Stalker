f8b1fb09f551dae500b7b00fb5ccaa5318b33e35
jordanr489 <jmr489@gcornell.edu>
2023-05-15 05:33:46 -0400

    Add descriptions to Stockinfo.mli

--- src/stockinfo.mli
+++ src/stockinfo.mli
@@ -6,7 +6,11 @@ type t
 val make : string * string * string * string -> t
 (**[make (comp, tt, amt, td)] creates a value of type t*)
 
+(**[company t] returns the company of a value of type t*)
 val company : t -> string
+(**[transaction_type t] returns the transaction_type of a value of type t*)
 val transaction_type : t -> string
+(**[amount t] returns the amount of a value of type t*)
 val amount : t -> string
+(**[trade_date t] returns the trade_date of a value of type t*)
 val trade_date : t -> string
